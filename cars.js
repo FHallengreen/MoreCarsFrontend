90d7f37df1350f4fcf2f0142dd1f7ecc1ab21a30
FHallengreen <fred70p9@stud.kea.dk>
2023-04-26 14:37:41 +0200

    first commit

---
+++ cars.js
@@ -0,0 +1,66 @@
+const SERVER_URL = "http://localhost:8080/api/";
+import { paginator } from "../../paginator/paginate-bootstrap.js";
+import { sanitizeStringWithTableRows } from "./utils.js";
+
+const SIZE = 10;
+const TOTAL = Math.ceil(1000 / SIZE); // Should come from the backend
+
+let cars = [];
+
+let sortField = "brand";
+let sortOrder = "desc";
+
+let initialized = false;
+
+function handleSort(pageNo) {
+  sortOrder = sortOrder == "asc" ? "desc" : "asc";
+  sortField = "brand";
+  load(pageNo);
+}
+
+async function load(pageNo) {
+  // We don't want to set up a new handler each time load fires
+  if (!initialized) {
+    document.getElementById("header-brand").onclick = function (evt) {
+      evt.preventDefault();
+      handleSort(pageNo);
+    };
+    initialized = true;
+  }
+
+  pageNo = Number(pageNo);
+
+  let queryString = `?sort=${sortField},${sortOrder}&size=${SIZE}&page=` + (pageNo - 1);
+  try {
+    cars = await fetch(`${SERVER_URL}cars${queryString}`).then((res) => res.json());
+  } catch (e) {
+    console.error(e);
+  }
+  const rows = cars
+    .map(
+      (car) => `
+  <tr>
+    <td>${car.id}</td>
+    <td>${car.brand}</td>
+    <td>${car.model}</td>
+    <td>${car.color}</td>
+    <td>${car.kilometers}</td>
+  `
+    )
+    .join("");
+
+  // DON'T forget to sanitize the string before inserting it into the DOM
+  document.getElementById("tbody").innerHTML = sanitizeStringWithTableRows(rows);
+
+  // (C1-2) REDRAW PAGINATION
+  paginator({
+    target: document.getElementById("car-paginator"),
+    total: TOTAL,
+    current: pageNo,
+    click: load,
+  });
+}
+
+
+// Load the first page initially
+load(1);
